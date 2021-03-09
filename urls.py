265dc11bf01782c143ac29d36d02f9cd9887e33b
Mauricio <mauriciofrias22@hotmail.com>
2021-03-08 23:33:11 -0500

    se crea repositorio de barra de busqueda

---
+++ urls.py
@@ -0,0 +1,7 @@
+from biblioteca  import views
+
+urlpatterns = [
+    # ...
+    url(r'^formulario-buscar/$', views.formulario_buscar),
+    # ...
+]
---
+++ views.py
@@ -0,0 +1,2 @@
+def formulario_buscar(request):
+    return render(request, 'formulario_buscar.html')
